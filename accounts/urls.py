24bc2f04f9ba619f3b85445ed7f9cce66f2b46b9
shinas07 <shinasamanpk@gmail.com>
2025-03-07 16:39:51 +0530

    refactor(auth): update token refresh URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('register/',RegisterView.as_view(),name='register'),
     path('login/',LoginView.as_view(),name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/',LogoutView.as_view(),name='logout')
     
 ]

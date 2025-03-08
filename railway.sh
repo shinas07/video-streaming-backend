47a45ea6d0ce52fddb88c170c6bf448e2721349a
shinas07 <shinasamanpk@gmail.com>
2025-03-08 17:02:24 +0530

    fix: correct  apt-get update

--- railway.sh
+++ railway.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-# Install missing OpenCV dependencies
-apt-get update && apt-get install -y libgl1-mesa-glx
+# Install missing system dependencies
+apt-get update && apt-get install -y \
+    libgl1-mesa-glx \
+    libglib2.0-0 \
+    libsm6 \
+    libxext6 \
+    libxrender1
 
-# Run migrations (optional)
+# Run migrations
 python manage.py migrate
 
 # Start Django
 gunicorn video_streaming.wsgi:application --bind 0.0.0.0:$PORT
+
+# # Install missing OpenCV dependencies
+# apt-get update && apt-get install -y libgl1-mesa-glx
+
+# # Run migrations (optional)
+# python manage.py migrate
+
+# # Start Django
+# gunicorn video_streaming.wsgi:application --bind 0.0.0.0:$PORT
